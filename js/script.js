0344368097f4f0e30f6877af7d891c219f08a3fa
Bokoffalexander <89662116+Bokoffalexander@users.noreply.github.com>
2025-06-07 22:51:17 +0300

    Update script.js swagger API

--- js/script.js
+++ js/script.js
@@ -5,20 +5,16 @@ let dat="Start";
 async function fetchLastPrice() {  
       const response = await fetch('https://petstore.swagger.io/v2/user/boko');  
       const data = await response.json();  
-	dat = data;
-	console.log(data);
-///////////////////
-              
-
+      dat = data;
+      console.log(data);
+}
+///////////////////     
       const para = document.querySelector('#write');
-
       para.addEventListener('click', updateName);
-
+///////////////////
       function updateName() {
         fetchLastPrice();
-        const name = prompt('Enter a new name');
-
-
-            
+        const name = prompt('Enter a new name');    
         para.textContent = `Your new name: ${name} and ${dat.email}`;
       }
+///////////////////
