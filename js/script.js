9db493392aafc5a2f28518bf4070946355dcbd3c
Bokoffalexander <89662116+Bokoffalexander@users.noreply.github.com>
2025-06-08 00:31:21 +0300

    Update script.js - google API

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 "use strict";
 ///////////////////
-let dat="Start";
+let d = "Start";
 ///////////////////
 async function fetchLastPrice() {  
-      const response = await fetch('https://petstore.swagger.io/v2/user/boko');  
+      const response = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=AehSKLhH2xvl5qD-cGmTvMPjLYYnoBOdpaMuAIaonsmjJpvEfm4PvlalovHnjeIakDQxm8ekBkXW98STIBkApdnFkl9mNK81HKV4qp3UNiiSwkFWdV4AUJhw7fT144xSL6o1HIByfGT8xeaElM30eAgAHeLvqXTADLX6XwucGKfwDQgMb_cqfSrK7Ob38DKcBe3Yl9OiE5gAYd4EyZAOVb-aBz8o9hhr0RKSeg7qddUNmA763VaVeu0yHYQ1tsPsLrBt0N-g62DpDPCuUjQjNXl6uurFjn_UdWrctCvhWHuO&lib=MZcsNb0Mnrg0JRLj0QmDrFb4Kw0A39Po-');  
       const data = await response.json();  
-      dat = data;
+      d = data;
       console.log(data);
 }
+/////////////////// 
+fetchLastPrice();
 ///////////////////     
       const para = document.querySelector('#write');
       para.addEventListener('click', updateName);
 ///////////////////
       function updateName() {
-        fetchLastPrice();
         const name = prompt('Enter a new name');    
-        para.textContent = `Your new name: ${name} and ${dat.email}`;
+        para.textContent = `Your new name: ${name} and ${d.data[1].Name}`;
       }
 ///////////////////
